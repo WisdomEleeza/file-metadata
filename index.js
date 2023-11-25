4b89705b675535addacda9f307cfe0726ea102fd
Wisdom Eleeza <whiy07@gmail.com>
2023-11-25 09:50:37 +0000

    add file upload function(10)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+const fs = require("fs");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
