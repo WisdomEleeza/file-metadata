4b6afa0b31bf48ddfcb6c01a3c50db43aa924d60
Wisdom Eleeza <whiy07@gmail.com>
2023-11-25 08:18:05 +0000

    add multer configuration for file upload

--- index.js
+++ index.js
@@ -6,31 +6,40 @@ require("dotenv").config();
 const Schema = mongoose.Schema;
 
 const connectDB = async () => {
-  const mongoose = require("mongoose");
-
-  const connectDB = async () => {
-    try {
-      await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log("Database connected");
-    } catch (error) {
-      console.log("Failed to connect to Database", error);
-    }
-  };
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected");
+  } catch (error) {
+    console.log("Failed to connect to Database", error);
+  }
 };
 
 connectDB();
 
 const imageFileSchema = new mongoose.Schema({
   image: {
-    type: String, 
+    type: String,
   },
 });
 
-const Image = mongoose.model("Image", imageFileSchema); 
+const Image = mongoose.model("Image", imageFileSchema);
 module.exports = Image;
+
+//Multer Configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 var app = express();
 
